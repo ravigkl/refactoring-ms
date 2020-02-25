2883d6867d9c67d657fdd5480e79d1edf98c6afa
ravi singh <ravi.shankar.singh@hotmail.com>
2020-02-25 11:51:29 +0530

    first commit

---
+++ microservice/user/src/main/java/org/javaee7/wildfly/samples/everest/uzer/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package org.javaee7.wildfly.samples.everest.uzer;
+
+import javax.ws.rs.core.Application;
+
+/**
+ * @author ravigkl
+ */
+//@EnableSnoopClient
+@javax.ws.rs.ApplicationPath("resources")
+public class ApplicationConfig extends Application {
+}
